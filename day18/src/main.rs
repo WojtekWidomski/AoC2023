5f561193fd29659ba7afe19dbfeacdfcd2e1a40f
Wojtek Widomski <98206524+WojtekWidomski@users.noreply.github.com>
2023-12-18 17:58:04 +0100

    remove commented ∨ unused code

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -6,13 +6,7 @@ const FILENAME: &str = "input";
 
 const ARR_SIZE: usize = 1000;
 
-#[derive(Clone, Debug)]
-enum TileDirection {
-    Vertical,
-    Horizontal,
-    Empty,
-}
-
+#[allow(unused)]
 fn print_arr(arr: &Vec<Vec<bool>>) {
     for l in arr {
         for i in l {
@@ -43,7 +37,6 @@ fn main() -> Result<()> {
     let mut x = ARR_SIZE / 2;
     let mut y = ARR_SIZE / 2;
 
-    // terrain[y][x] = TileDirection::Horizontal;
     terrain[y][x] = true;
 
     for (dir, length) in plan {
@@ -51,37 +44,30 @@ fn main() -> Result<()> {
             'R' => {
                 for _ in 0..length {
                     x += 1;
-                    // terrain[y][x] = TileDirection::Horizontal;
                     terrain[y][x] = true;
                 }
             }
             'L' => {
                 for _ in 0..length {
                     x -= 1;
-                    // terrain[y][x] = TileDirection::Horizontal;
                     terrain[y][x] = true;
                 }
             }
             'U' => {
                 for _ in 0..length {
                     y -= 1;
-                    // terrain[y][x] = TileDirection::Vertical;
                     terrain[y][x] = true;
                 }
             }
             'D' => {
                 for _ in 0..length {
                     y += 1;
-                    // terrain[y][x] = TileDirection::Vertical;
                     terrain[y][x] = true;
                 }
             }
             _ => {}
         }
-        // terrain[y][x] = true;
     }
-
-    // print_arr(&terrain);
 
     let mut counting_enabled = false;
     let mut result = 0;
